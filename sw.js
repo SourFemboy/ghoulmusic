a9bb0eee256f89ca1a121077200b23452a15ab8d
SourFemboy <sourfemboy@gmail.com>
2026-09-05 19:27:58 -0700

    Replace

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE="ghoulmusic-shell-v1";
+const CACHE="ghoulmusic-shell-v2-blood";
 const ASSETS=[
   "./","./index.html","./styles.css","./db.js","./metadata.js","./app.js",
-  "./manifest.webmanifest","./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"
+  "./manifest.webmanifest","./background.jpg","./icons/icon-180.png","./icons/icon-192.png","./icons/icon-512.png"
 ];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
